3d4214fc65bc1ea8296a8384f7c4f989a6ae7eef
jkim68888 <jkim68888@gmail.com>
2022-12-05 18:42:05 +0900

    [fix] 학생 추가, 삭제 기능 수정 및 데이터 모델 작성

--- MyCreditManager_jkim68888/MyCreditManager_jkim68888/main.swift
+++ MyCreditManager_jkim68888/MyCreditManager_jkim68888/main.swift
@@ -7,9 +7,29 @@
 
 import Foundation
 
+struct StudentsGrade {
+	var student: String
+	var subject: String
+	var score: Score
+	var grade: Int
+}
+
+enum Score: String {
+	case A
+	case A0
+	case B
+	case B0
+	case C
+	case C0
+	case D
+	case D0
+	case F
+}
+
 let menu = "원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료"
 let inputWraning = "입력이 잘못되었습니다. 다시 확인해주세요."
-var students: [String] = []
+
+var studentsGrades: [StudentsGrade] = []
 
 func selectMenu() {
 	print(menu)
@@ -36,19 +56,21 @@ func selectMenu() {
 
 func addStudent() {
 	print("추가할 학생의 이름을 입력해주세요")
-	
+
 	if let userInput = readLine() {
 		if userInput == "" {
 			print(inputWraning)
 			selectMenu()
-		} else if students.contains(userInput) {
+		} else if studentsGrades.contains(where: { StudentsGrade in
+			StudentsGrade.student == userInput
+		}) {
 			print("\(userInput)은 이미 존재하는 학생입니다. 추가하지 않습니다.")
-			print("⭐️",students)
+			print("⭐️",studentsGrades)
 			selectMenu()
 		} else {
-			students.append(userInput)
+			studentsGrades.append(StudentsGrade(student: userInput, subject: "", score: .F, grade: 0))
 			print("\(userInput) 학생을 추가했습니다.")
-			print("⭐️",students)
+			print("⭐️",studentsGrades)
 			selectMenu()
 		}
 	}
@@ -61,14 +83,16 @@ func deleteStudent() {
 		if userInput == "" {
 			print(inputWraning)
 			selectMenu()
-		} else if students.contains(userInput) {
-			students = students.filter { $0 != userInput }
+		} else if studentsGrades.contains(where: { StudentsGrade in
+			StudentsGrade.student == userInput
+		}) {
+			studentsGrades = studentsGrades.filter { $0.student != userInput }
 			print("\(userInput) 학생을 삭제하였습니다.")
-			print("⭐️",students)
+			print("⭐️",studentsGrades)
 			selectMenu()
 		} else {
 			print("\(userInput) 학생을 찾지 못했습니다.")
-			print("⭐️",students)
+			print("⭐️",studentsGrades)
 			selectMenu()
 		}
 	}
